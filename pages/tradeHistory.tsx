e6699d4bd825d22c7c5df7130f5b7670bb88dd44
superoverflow <me@superoverflow.com>
2022-07-03 15:30:03 +0100

    add table

--- pages/tradeHistory.tsx
+++ pages/tradeHistory.tsx
@@ -1,4 +1,4 @@
-import { Text, LoadingOverlay, Group } from "@mantine/core"
+import { Table, LoadingOverlay, Group } from "@mantine/core"
 import type { NextPage } from "next"
 import axios from "axios"
 import { useQuery } from "react-query"
@@ -9,17 +9,62 @@ const fetchTradeHistory = async () => {
   return data
 }
 
+const cols = [
+  {
+    header: "Date",
+  },
+  {
+    header: "B/S",
+  },
+  {
+    header: "Crypto",
+  },
+  {
+    header: "Amount",
+  },
+  {
+    header: "Fiat Currency",
+  },
+  {
+    header: "Fiat Amount",
+  },
+  {
+    header: "Fee",
+  },
+]
+
 const TradeHistory: NextPage = () => {
   const { isLoading, data } = useQuery("trades", fetchTradeHistory)
-  
+  const tableHeader = (
+    <thead>
+      <tr>
+        {cols.map((col, index) => (
+          <th key={index}>{col.header}</th>
+        ))}
+      </tr>
+    </thead>
+  )
+  const rows = data?.map((trade) => (
+    <tr key={trade.id}>
+      <td>{trade.date}</td>
+      <td>{trade.buySellIndicator}</td>
+      <td>{trade.crypto}</td>
+      <td>{trade.amount}</td>
+      <td>{trade.fiatCurrency}</td>
+      <td>{trade.fiatAmount}</td>
+      <td>
+        {trade.feeCurrency} {trade.feeAmount}
+      </td>
+    </tr>
+  ))
+
   return (
     <Group>
       <LoadingOverlay visible={!!isLoading} />
-      {data?.map((trade) => (
-        <Text key={trade.id} size="md">
-          {trade.id}
-        </Text>
-      ))}
+      <Table>
+        {tableHeader}
+        <tbody>{rows}</tbody>
+      </Table>
     </Group>
   )
 }
