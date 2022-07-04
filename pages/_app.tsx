2a1495be5692761d6afa0c0d4a4e95cc92682007
superoverflow <me@superoverflow.com>
2022-07-05 00:01:14 +0100

    update page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App(props: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Head>
-        <title>Page title</title>
+        <title>Moneybox</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
