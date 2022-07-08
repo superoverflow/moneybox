d9d5c101070edce77bb0299bb54205d20bcd7bad
superoverflow <me@superoverflow.com>
2022-07-08 11:18:18 +0100

    add new favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app"
-import { Router, useRouter } from "next/router"
+import { useRouter } from "next/router"
 import Head from "next/head"
 import {
   MantineProvider,
@@ -25,6 +25,7 @@ export default function App(props: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <link rel="icon" href="favicon.png"></link>
       </Head>
 
       <MantineProvider
